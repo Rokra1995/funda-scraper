34e3ffa70c2d83e1daf3e4a17a4cb70541a38ba6
Rokra1995 <rkratschmayr@me.com>
2020-12-10 12:14:44 +0100

    Repo finalized for hand in

--- Final scraping logic/funda/funda/exporters.py
+++ Final scraping logic/funda/funda/exporters.py
@@ -1,7 +1,8 @@
 from scrapy.exporters import CsvItemExporter
 from funda.settings import CSV_SEP
 
-
+#Custom exporter to be able to change the seperator in the settings.py file
+# © Robin Kratschmayr
 class CsvCustomSeperator(CsvItemExporter):
     def __init__(self, *args, **kwargs):
         kwargs['encoding'] = 'utf-8'
